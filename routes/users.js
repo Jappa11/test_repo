c91a69a3fe79d9999384b1b4b6deb3d3c4b9dd7f
Hamza <hamza@zembratech.com>
2022-04-23 15:00:15 -0700

    Cors setup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
-var router = express.Router();
+var usersRouter = express.Router();
+const cors = require('./cors');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+usersRouter.route('/')
+// GET 
+.get(cors.corsWithOptions, (req, res, next) => {
+  try {
+    res.send('respond with a resource');
+  }
+  catch (err) {
+    new Error(err.message)
+  }
+})
 
-module.exports = router;
+// POST 
+.post(cors.corsWithOptions, async (req, res, next) => {
+  // DEBUGGER 
+  try {
+    console.log(`request body: ${req.body}`)
+    console.log(`request email: ${req.body.email}`)
+    console.log(`request password: ${req.body.password}`)
+    if(!req.body) {
+      console.error('missing request body')
+      res.send('missing request body')
+    }
+    if (!req.body.email || !req.body.password) {
+      console.error('check request');
+      res.send('check request');      
+    }
+    else {
+      // res.send('request accepted');
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 200;
+      res.json(req.body);
+    }
+  }
+  catch (err) {
+    console.error(err.message)
+  }
+  // END DEBUGGIN
+})
+
+
+
+
+
+
+
+
+module.exports = usersRouter;
